697653a8d96730eb8dee6a687c70602370881ae7
Mitrofanov Ivan <vanyok98@mail.ru>
2022-12-06 17:01:16 +0300

    Доделана регулировка громкости звука, исправлен перезапуск игры

--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -6,21 +6,28 @@ public class Volume : MonoBehaviour
     [SerializeField] private AudioMixer mixer;
     [SerializeField] private VolumeView volumeView;
 
-    private const float Muted = -60f;
+    private const float Muted = -80f;
     private float volume = 0f;
     private float lastVolume = 0f;
 
     private bool isMuted = false;
 
+    private void Start()
+    {
+        mixer.GetFloat("MasterVolume", out float masterVolume);
+        volume = Mathf.InverseLerp(Muted, 0f, masterVolume);
+        volumeView.SetSliderValue(volume);
+    }
+
     public void Change(float value)
     {
         isMuted = value == 0;
         volume = Mathf.Lerp(Muted, 0f, value);
-        SetMasterVolume();
+        SetMasterVolume(volume);
         volumeView.SetIcon(isMuted);
     }
 
-    private void SetMasterVolume() => mixer.SetFloat("MasterVolume", volume);
+    private void SetMasterVolume(float value) => mixer.SetFloat("MasterVolume", value);
 
     public void ToggleMute()
     {
@@ -35,7 +42,7 @@ public class Volume : MonoBehaviour
             volume = Muted;
             volumeView.SetSliderValue(0f);
         }
-        SetMasterVolume();
+        SetMasterVolume(volume);
         volumeView.SetIcon(isMuted);
     }
 }
